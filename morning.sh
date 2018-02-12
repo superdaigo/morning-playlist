03de192b9505446e3f8070a9ad35faee00ec299a
Daigo Sakamoto <superdaigo@gmail.com>
2018-02-12 12:09:54 +0900

    add --quit-terminal option

--- morning.sh
+++ morning.sh
@@ -17,21 +17,28 @@ if [ ! -z $1 ] && [[ $1 =~ ^[0-9]{1,2}:[0-9]{2}$ ]] ; then
   fi
 fi
 
+QUIT_TERMINAL=0
+if [ ! -z $2 ] && [ $2 == "--quit-terminal" ] ; then
+  QUIT_TERMINAL=1
+fi
+
 BASE_DIR=`python -c 'import os,sys;print(os.path.dirname(os.path.realpath(sys.argv[1])))' "${0}"`
 
 function start {
   # start vlc
-  open -g -a "VLC" --background "${BASE_DIR}/playlist.xspf"
+  open -g -a "VLC" --background "${BASE_DIR}/playlist.m3u"
   # count every 10 min this script stops until STOP_AT time
-  alert_10_minutes.sh ${STOP_AT}
+  ${BASE_DIR}/alert_10_minutes.sh ${STOP_AT}
 }
 
 function stop {
   # stop vlc
   osascript -e 'quit app "VLC"'
 
-  # stop terminal after finishing this script
-  sleep 3 && osascript -e 'quit app "Terminal"' &
+  if [ $QUIT_TERMINAL -eq 1 ] ; then
+    # stop terminal after finishing this script
+    sleep 3 && osascript -e 'quit app "Terminal"' &
+  fi
 }
 
 start
